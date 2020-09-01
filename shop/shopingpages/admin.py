0cdafa4874832b5d2776fea105ec9bfaefb15d13
Ihor <igorechekqwerty@gmail.com>
2020-09-02 00:46:31 +0300

    create model add goods

--- shop/shopingpages/admin.py
+++ shop/shopingpages/admin.py
@@ -1,3 +1,8 @@
+from django import forms
 from django.contrib import admin
+from .models import Good
 
-# Register your models here.
+class GoodsAdminForm(admin.ModelAdmin):
+    list_display = ("name", "price", "color", "description", "image")
+    list_display_links = ("name",)
+admin.site.register(Good, GoodsAdminForm)
--- shop/shopingpages/models.py
+++ shop/shopingpages/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Good(models.Model):
+    name = models.CharField("Назва товару", max_length=50)
+    price = models.IntegerField("Ціна товару", max_length=12)
+    color = models.CharField("Колір", max_length=20)
+    description = models.TextField("Опис товару", max_length=250)
+    image = models.ImageField(upload_to='photos/%Y/%m/%d/')
+
+    def __str__(self):
+        return self.name
